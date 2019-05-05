a458c2256bc49eccabf719eda6b0df3c4e0e8d59
javier Martinez <kiloman.x@gmail.com>
2019-05-05 16:21:23 -0700

    added player script to look, and jump

---
+++ FPS_Project/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+  // editable stats
+  public float jumpSpeed = 4;
+  public float runSpeed = 4;
+  public float playerHp = 100;
+  public bool isMain = true;
+
+  // values
+  private float _camSens = 0.25f; //How sensitive it with mouse
+  // float airAccRatio = 5;
+
+  // state
+  // [HideInInspector]
+  private Vector3 _velocity = new Vector3();
+  private Vector3 _inputVelocity = new Vector3();
+  private Vector3 _lastMouse = new Vector3(255, 255, 255);
+  private bool _jumpPressed = false;
+  private bool _jumpHold = false;
+
+  // unity reference
+  public CharacterController charControl;
+  public Transform camera;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    //Mouse  camera angle
+    _lastMouse = Input.mousePosition - _lastMouse;
+    _lastMouse = new Vector3(-_lastMouse.y * _camSens, _lastMouse.x * _camSens, 0);
+    _lastMouse = new Vector3(transform.eulerAngles.x + _lastMouse.x, transform.eulerAngles.y + _lastMouse.y, 0);
+    transform.eulerAngles = _lastMouse;
+    _lastMouse = Input.mousePosition;
+
+    if (charControl.isGrounded)
+    {
+      _velocity.y = 0;
+      if (_jumpPressed) {
+        _velocity.y = jumpSpeed;
+      }
+    } else {
+
+    }
+      
+    _velocity.x = 0;
+    _velocity.z = 0;
+    _velocity += GetBaseInput() * runSpeed;
+
+    _velocity += Physics.gravity * Time.deltaTime;
+    
+    charControl.Move(_velocity * Time.deltaTime );
+
+    Camera.main.transform.position = camera.position;
+    Camera.main.transform.rotation = camera.rotation;
+  }
+
+  private Vector3 GetBaseInput()
+  { 
+    _jumpPressed = Input.GetButtonDown("Jump");
+    _jumpHold = Input.GetButton("Jump");
+
+    //returns the basic values, if it's 0 than it's not active.
+    Vector3 moveDirection = new Vector3();
+    moveDirection += Input.GetAxis("Vertical") * transform.forward
+    + Input.GetAxis("Horizontal") * transform.right;
+    moveDirection.y = 0;
+    moveDirection.Normalize();
+    return moveDirection;
+  }
+
+  void SetCursorState()
+  {
+    Cursor.lockState = CursorLockMode.Locked;
+  }
+}
