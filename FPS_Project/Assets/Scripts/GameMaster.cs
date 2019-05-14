843b0cedc712e296af81ebc7275442b549a62de5
Smireles <mireles001@gmail.com>
2019-05-14 00:00:23 -0700

    Main menu ready, working in limbo and character select screen

---
+++ FPS_Project/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMaster : MonoBehaviour
+{
+  public string username;
+  private string _myIp;
+  private string _serverIp;
+  private bool _isServer = true;
+
+  public void StartUp(bool isServer, string myIp, string serverIp, string name)
+  {
+    username = name;
+    _myIp = myIp;
+    _serverIp = serverIp;
+    _isServer = isServer;
+
+    Debug.Log("Is Server? " + _isServer);
+  }
+
+  public string ServerIp
+  {
+    get
+    {
+      return _serverIp;
+    }
+  }
+
+  public bool IsServer
+  {
+    get
+    {
+      return _isServer;
+    }
+  }
+}
--- FPS_Project/Assets/Scripts/UserInteface/MenuManager.cs
+++ FPS_Project/Assets/Scripts/UserInteface/MenuManager.cs
@@ -1,8 +1,12 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+  [SerializeField]
+  private GameObject _gameMaster;
+
   [SerializeField]
   private Image _fader;
   [SerializeField]
@@ -16,6 +20,26 @@ public class MenuManager : MonoBehaviour
 
   [SerializeField]
   private Button _exit;
+  [SerializeField]
+  private Button _goToCreate;
+  [SerializeField]
+  private Button _goToJoin;
+  [SerializeField]
+  private Button _goBackJoin;
+  [SerializeField]
+  private Button _goBackCreate;
+  [SerializeField]
+  private Button _createServer;
+  [SerializeField]
+  private Button _joinServer;
+  [SerializeField]
+  private InputField _serverIp;
+  [SerializeField]
+  private InputField _connectIp;
+  [SerializeField]
+  private InputField _setNameServer;
+  [SerializeField]
+  private InputField _setNameClient;
 
   private Animator _anim;
   private AudioSource _bg;
@@ -23,25 +47,40 @@ public class MenuManager : MonoBehaviour
   private bool _killApp = false;
   private bool _goToLimbo = false;
 
+  private string _username = "NewPlayer";
+  private string _myIp;
+  private string _ip;
+  private bool _isServer = false;
+
   private void Start()
   {
     _fader.gameObject.SetActive(true);
-    _title.SetActive(true);
+    _title.SetActive(false);
     _create.SetActive(false);
     _join.SetActive(false);
 
     _exit.onClick.AddListener(delegate () { BtnHandler("exit"); });
+    _goToCreate.onClick.AddListener(delegate () { BtnHandler("goToCreate"); });
+    _goToJoin.onClick.AddListener(delegate () { BtnHandler("goToJoin"); });
+    _goBackJoin.onClick.AddListener(delegate () { BtnHandler(); });
+    _goBackCreate.onClick.AddListener(delegate () { BtnHandler(); });
+
+    _createServer.onClick.AddListener(delegate () { BtnHandler("create"); });
+    _joinServer.onClick.AddListener(delegate () { BtnHandler("join"); });
 
     _anim = GetComponent<Animator>();
     _bg = GetComponent<AudioSource>();
     _buttonsSFX = gameObject.AddComponent<AudioSource>();
     _buttonsSFX.playOnAwake = false;
     _buttonsSFX.clip = _click;
-  }
 
-  public void FadeOutCb()
-  {
-    _fader.gameObject.SetActive(false);
+    _anim.SetBool("at_create", false);
+    _anim.SetBool("at_join", false);
+
+    _serverIp.readOnly = true;
+    _myIp = _serverIp.text = new IP().GetIp();
+
+    CheckForGameMaster();
   }
 
   private void FadeInCb()
@@ -53,22 +92,76 @@ public class MenuManager : MonoBehaviour
     }
     else if (_goToLimbo)
     {
-      Debug.Log("Go to Limbo");
+      GoToLimbo();
     }
   }
 
-  private void BtnHandler(string btnPressed)
+  private void BtnHandler(string btnPressed = "goBack")
   {
     _buttonsSFX.Play();
     switch (btnPressed)
     {
       case "exit":
         _killApp = true;
-        _fader.gameObject.SetActive(true);
         _anim.Play("fadeIn");
+        break;
+      case "goToCreate":
+        _anim.SetBool("at_create", true);
+        _anim.SetBool("at_join", false);
+        break;
+      case "goToJoin":
+        _anim.SetBool("at_join", true);
+        _anim.SetBool("at_create", false);
+        break;
+      case "create":
+        _isServer = true;
+        _goToLimbo = true;
+        SetName(_setNameServer.text);
+        _anim.Play("fadeIn");
+        break;
+      case "join":
+        if (_connectIp.text.Length > 0)
+        {
+          _isServer = false;
+          _goToLimbo = true;
+          _ip = _connectIp.text;
+          SetName(_setNameClient.text);
+          _anim.Play("fadeIn");
+        }
+        break;
+      default:
+        _anim.SetBool("at_join", false);
+        _anim.SetBool("at_create", false);
         break;
     }
   }
 
-  
+  private void SetName(string name)
+  {
+    if (name.Length > 0)
+    {
+      _username = name;
+    }
+  }
+
+  public void GoToLimbo()
+  {
+    GameObject master = Instantiate(_gameMaster);
+    master.name = "GameMaster";
+    DontDestroyOnLoad(master);
+
+    master.GetComponent<GameMaster>().StartUp(_isServer, _myIp, _ip, _username);
+
+    SceneManager.LoadScene("limbo");
+  }
+
+  private void CheckForGameMaster()
+  {
+    GameObject master = GameObject.FindGameObjectWithTag("GameMaster");
+
+    if (master)
+    {
+      Destroy(master);
+    }
+  }
 }
