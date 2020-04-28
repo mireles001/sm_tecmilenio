556f9d816f5c34e0ecca060441ec4153b9b85091
Smireles <mireles001@gmail.com>
2020-04-28 08:21:01 -0700

    Changes in repo

---
+++ 2020_ENE-MAY/Platformer2D/Platformer2D/Assets/PickUpController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PickUpController : MonoBehaviour
+{
+  public GameObject particulas;
+
+  private void OnTriggerEnter2D(Collider2D collision)
+  {
+    if (collision.gameObject.tag == "Player")
+    {
+      Instantiate(particulas).transform.position = transform.position;
+      Destroy(gameObject);
+    }
+  }
+}
