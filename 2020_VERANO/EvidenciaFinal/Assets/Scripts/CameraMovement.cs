8208d46547297c57843ff09a5cd7615ba8cf1643
Smireles <mireles001@gmail.com>
2020-07-13 18:23:12 -0700

    Adding Verano material and Evidencia Final unity project for summer course.

---
+++ 2020_VERANO/EvidenciaFinal/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+  [SerializeField]
+  private Transform _target = null;
+  [SerializeField]
+  private float _damp = 1;
+  private Vector3 _velocity;
+
+  private void Update()
+  {
+    Vector3 newPosition = transform.position;
+    newPosition.x = _target.position.x;
+
+    if (_damp <= 0)
+    {
+      transform.position = newPosition;
+    }
+    else
+    {
+      transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref _velocity, _damp);
+    }
+  }
+}
---
+++ 2020_VERANO/EvidenciaFinal/Assets/Scripts/CharacterAnimation.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CharacterAnimation : MonoBehaviour
+{
+  private Animator _anim;
+  private SpriteRenderer _spriteRenderer;
+  private PlayerMovement _movement;
+
+  private void Start()
+  {
+    _anim = GetComponent<Animator>();
+    _spriteRenderer = GetComponent<SpriteRenderer>();
+    _movement = GetComponent<PlayerMovement>();
+  }
+
+  private void Update()
+  {
+    if (_anim) _anim.SetBool("isMoving", _movement.Direction != 0);
+
+    if (_movement.Direction == 0) return;
+
+    if (_spriteRenderer) _spriteRenderer.flipX = (_movement.Direction < 0);
+  }
+}
