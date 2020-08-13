3ecdb2f7c4172405b35f528fab721b14165a405a
Smireles <mireles001@gmail.com>
2020-08-12 23:23:47 -0700

    Evidencia final y archivos base para clase.

---
+++ 2020_VERANO/EvidenciaFinal/Assets/Scripts/CloseAppAction.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CloseAppAction : MonoBehaviour
+{
+  private void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.Escape))
+    {
+      Debug.Log("Quit Application");
+      Application.Quit();
+    }
+  }
+}
